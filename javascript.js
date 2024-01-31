11d6b699dfb055f104baac542aa410ab79d5497e
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2024-01-30 21:35:33 -0300

    adicionando Js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let vasco = [17][17];
+
+/* Direita i aumenta, esquerda i diminui, pra baixo j aumenta, pra cima j diminui */
+
+for(let i = 0; i <17 ; i++){    
+    for(let j = 0; j < 17; j++){
+        console.log(i + ' - ' + j);
+    }
+    console.log('\n');
+}   
